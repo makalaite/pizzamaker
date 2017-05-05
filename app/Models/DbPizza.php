365623dc5f2c6f44f0fad384710edd3e3ae42b5b
Agne <agnepetrylaite@gmail.com>
2017-05-05 11:38:28 +0300

    relationships

--- app/Models/DbPizza.php
+++ app/Models/DbPizza.php
@@ -8,4 +8,29 @@ class DbPizza extends CoreModel
     protected $table = 'db_pizza';
 
     protected $fillable = ['id', 'cheese_id', 'base_id', 'comment', 'client_name'];
+
+    public function orderIngredientConnection()
+    {
+        return $this->belongsToMany(DbIngredients::class, 'db_pizza_ingredients_connections',
+            'pizza_id', 'ingredient_id');
+    }
+
+    public function ingredients()
+    {
+        return $this->hasMany(DbPizzaIngredientsConnections::class, 'pizza_id',
+            'id')->with(['IngredientsData']);
+    }
+
+    public function cheese()
+    {
+        return $this->hasOne(DbCheeses::class, 'id',
+            'cheese_id');
+    }
+
+    public function base()
+    {
+        return $this->hasOne(DbBase::class, 'id',
+            'base_id');
+    }
+
 }
