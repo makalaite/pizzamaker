8b3409ff3b3ffa44f6d7ec95029396d71d4b9562
Agne <agnepetrylaite@gmail.com>
2017-05-05 11:40:55 +0300

    ingredients data function

--- app/Models/DbPizzaIngredientsConnections.php
+++ app/Models/DbPizzaIngredientsConnections.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 
-class DbPizzaIngredientsConnections extends CoreModel
+class DbPizzaIngredientsConnections extends Model
 {
+    protected $updated_at = false;
+
     protected $table = 'db_pizza_ingredients_connections';
 
-    protected $fillable = ['id', 'pizza_id', 'ingredient_id'];
+    protected $fillable = ['pizza_id', 'ingredient_id'];
+
+    public function IngredientsData()
+    {
+        return $this->hasOne(DbIngredients::class, 'id', 'ingredient_id');
+    }
 }
